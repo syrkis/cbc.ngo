ac22e2a2c3aa87c253a2ce235e7c9ff85be9244c
Noah Syrkis <noah@syrkis.com>
2023-09-07 13:12:25 -0300

    multilingual

--- src/lib/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { register, init, getLocaleFromNavigator } from "svelte-i18n";
-
-register("en", () => import("../lang/en.json"));
-register("pt", () => import("../lang/pt.json"));
-
-init({
-  fallbackLocale: "en",
-  initialLocale: getLocaleFromNavigator()
-});
